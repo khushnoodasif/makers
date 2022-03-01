a7ba5c6f6231f1a39de81949f4b229359598de7b
Khushnood Asif <72355837+khushnoodasif@users.noreply.github.com>
2022-03-01 20:11:18 +0000

    Create customer-counter.rb

---
+++ week-1-and-2-basic-programming/customer-counter.rb
@@ -0,0 +1,24 @@
+puts "Welcome to the Supermarket Customer Counter!"
+
+num_of_customers = 0
+input = ""
+
+while input.downcase != "exit"
+    print "Enter '+', '-' or 'exit':  "
+    input = gets.chomp
+    if input == "+"
+        puts "Total Customers: #{num_of_customers += 1}."
+    elsif input == "-"
+        num_of_customers -= 1
+        if num_of_customers >= 0
+            puts "Total Customers: #{num_of_customers}."
+        else
+            puts "ERROR: Customer number cannot be negative."
+            num_of_customers = 0
+        end
+    elsif input.downcase == "exit"
+        puts "Exiting Program."
+    else input.downcase != "exit"
+        puts "ERROR: Invalid Text."
+    end
+end
