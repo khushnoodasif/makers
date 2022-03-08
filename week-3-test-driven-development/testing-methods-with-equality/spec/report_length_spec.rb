a437490a236566f33f45faecc45f8b720843e433
Khushnood Asif <khushnood99@gmail.com>
2022-03-08 10:42:06 +0000

    update directory

--- week-3-test-driven-development/testing-methods-with-equality/spec/report_length_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'report_length'
-
-RSpec.describe "report length method" do
-  it "returns length of the string" do
-    result = report_length("khushnood")
-    expect(result).to eq "This string was 9 characters long."
-  end
-end
