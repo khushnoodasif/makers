fc53077b90c2467f0a6eada1b0732e9b1ce12906
Khushnood Asif <khushnood99@gmail.com>
2022-03-08 14:23:14 +0000

    improve grammar complete

--- week-3-and-4-golden-square/design-single-method-program/design-recipe-improve-grammar/spec/improve_grammar_spec.rb
+++ week-3-and-4-golden-square/design-single-method-program/design-recipe-improve-grammar/spec/improve_grammar_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe "improve_grammar method" do
     expect{improve_grammar("")}.to raise_error "Please enter a sentence"
   end
 
-  it "returns zero for a empty string" do
+  it "returns true for sentence" do
     check_sentence = improve_grammar("Hello, world!")
     expect(check_sentence).to eq true
   end
 
-  it "returns zero for a empty string" do
+  it "returns false for sentence" do
     check_sentence = improve_grammar("hello, world!")
     expect(check_sentence).to eq false
   end
 
-  it "returns zero for a empty string" do
+  it "returns false for sentence" do
     check_sentence = improve_grammar("Hello, world")
     expect(check_sentence).to eq false
   end
 
-  it "returns zero for a empty string" do
+  it "returns true for sentence" do
     check_sentence = improve_grammar("Hello, world?")
     expect(check_sentence).to eq true
   end
