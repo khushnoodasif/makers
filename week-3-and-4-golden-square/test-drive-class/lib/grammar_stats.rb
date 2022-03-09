90539af274223b026e224f845a891aa1cb4304b5
Khushnood Asif <khushnood99@gmail.com>
2022-03-09 15:23:11 +0000

    test drive class complete

--- week-3-and-4-golden-square/test-drive-class/lib/grammar_stats.rb
+++ week-3-and-4-golden-square/test-drive-class/lib/grammar_stats.rb
@@ -1,15 +1,22 @@
 class GrammarStats
   def initialize
-    # ...
+    @texts_checked = 0
+    @texts_passed = 0
   end
 
-  def check(text) # text is a string
-    # Returns true or false depending on whether the text begins with a capital
-    # letter and ends with a sentence-ending punctuation mark.
+  def check(text)
+    if captial = text[0] == text[0].upcase
+      @texts_checked += 1
+      if sentence_ending = text[-1] == "." || text[-1] == "!" || text[-1] == "?"
+        @texts_checked += 1
+        @texts_passed += 1
+        return true
+      end
+    end
+    false
   end
 
   def percentage_good
-    # Returns as an integer the percentage of texts checked so far that passed
-    # the check defined in the `check` method. The number 55 represents 55%.
+    ((@texts_passed.to_f / @texts_checked.to_f) * 100).to_i
   end
 end
