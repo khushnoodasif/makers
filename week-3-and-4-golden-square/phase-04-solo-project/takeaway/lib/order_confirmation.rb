c9773ce37bcc1ac44924f13a88480e35d2332729
Khushnood Asif <khushnood99@gmail.com>
2022-03-17 18:05:19 +0000

    order confirmation class complete

--- week-3-and-4-golden-square/phase-04-solo-project/takeaway/lib/order_confirmation.rb
+++ week-3-and-4-golden-square/phase-04-solo-project/takeaway/lib/order_confirmation.rb
@@ -1,9 +1,23 @@
+require 'rubygems'
+require 'twilio-ruby'
+
 class OrderConfirmation
-  def initialize(receipt)
-    @receipt = receipt
+  def initialize(requester)
+    @requester = requester
   end
 
-  def send_text(number)
-    puts "#{number}: Thank you! Your order was placed and will be delivered before 18:52"
+  def send_text(account_sid, auth_token, from, to, body)
+    account_sid = ENV['account_sid']
+    auth_token = ENV['auth_token']
+    @client = @requester.new account_sid, auth_token
+    @client.account.messages.create(
+      from: '+441234123456',
+      to: @requester.number_request,
+      body: "Thank you for your order! Your total is £#{@requester.total}. Your order will be delivered before #{time}."
+    )
+  end
+
+  def time
+    Time.now.strftime("%H:%M")
   end
 end
