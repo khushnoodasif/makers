940e9ef864b470ac0289565094dca0b3b1adf8c0
Khushnood Asif <khushnood99@gmail.com>
2022-03-22 14:38:28 +0000

    changes

--- week-5-the-web/02-birthday-app/index.js
+++ week-5-the-web/02-birthday-app/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function () {
   let today = new Date();
   document.getElementById("bday").max = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + (today.getDate() < 10 ? ("0" + (today.getDate()-1)) : (today.getDate()-1));
@@ -25,34 +24,3 @@ function calculateDays() {
   } else {
     document.getElementById("days-left").innerText = "Please enter a valid birtday.";
   }
-  
-=======
-(function () {
-  let today = new Date();
-  document.getElementById("bday").max = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + (today.getDate() < 10 ? ("0" + (today.getDate()-1)) : (today.getDate()-1));
-})();
-
-function calculateDays() {
-  let name = document.getElementById("name").value;
-  let today = new Date();
-  let bday = new Date(document.getElementById("bday").value); 
-  let age = today.getFullYear() - bday.getFullYear();
-  
-  let upcomingBday = new Date(today.getFullYear(), bday.getMonth(), bday.getDate());
-  
-  if(today > upcomingBday) {
-    upcomingBday.setFullYear(today.getFullYear() + 1);
-  }
-  
-  var one_day = 24 * 60 * 60 * 1000;
-  
-  let daysLeft = Math.ceil((upcomingBday.getTime() - today.getTime()) / (one_day));
-  
-  if (daysLeft && age < 200) {
-    document.getElementById("days-left").innerText = name + ` In ${daysLeft} day(s), you will be ${age}!`;  
-  } else {
-    document.getElementById("days-left").innerText = "Please enter a valid birtday.";
-  }
-  
->>>>>>> b1748959923c0edfb4f15441de31b977473e7f5b
-}
